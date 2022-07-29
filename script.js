399932acc676848b075663d4d9ca603f68785afe
Caleb <calebfunderburk@icloud.com>
2022-07-29 15:27:31 -0400

    added variables to js

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-// Assignment Code
 // Generate Button
 var generateBtn = document.querySelector("#generate");
 
 //Generate a password
 function generatePassword() {
   console.log("Button working");
-  var promptLength = window.prompt('How many characters would you like your password to be?') 
+
+  //  Promt the user for the password criteria
+  var promptLength = window.prompt("How many characters would you like your password to be?");
+
+    // Password must be 8 characters long
     if (promptLength < 8) {
-      window.alert("Your password must be at least 8 characters long.")
+      window.alert("Your password must be at least 8 characters long.");
     }
 
+    // Password must not be longer than 12 characters long  
     if (promptLength > 128) {
-      window.alert("Your password cannot be longer than 128 characters long.")
+      window.alert("Your password cannot be longer than 128 characters long.");
     }
-  
 
-//  1. Promt the user for the password criteria
-//    A.) Password length 8 < 128
-//    B.) Lowercase, uppercase, or special characters?
+  // Option for lowercase characters  
+  var confirmLowercase = window.confirm("Would you like to include Lowercase characters?");
+
+    // If yes, use lowercase characters
+
+    // If no, don't 
+
+  // Option for uppercase characters
+  var confirmUppercase = window.confirm("Would you like to include Uppercase characters?");
+
+  // Option for special characters
+  var confirmSpecial = window.confirm("Would you like to include Special characters?");
+
 //  2. Validate the input (make sure the length is between 8 and 128, at least 1 character type is being selected for the password)
 //  3. Generate password based off criteria
 
   // Display the password to the page
-  return "Generated password"
+  return
 }
 
 // Write password to the #password input
